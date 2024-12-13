c8af76b298c3da2e4716b4b865f4854c2b53ea16
KBalamuralikrishna <119946302+KBalamuralikrishna@users.noreply.github.com>
2024-12-13 06:56:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Smooth scrolling for navigation links
+    $('nav ul li a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
